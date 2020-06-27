dd3f9cea248c8309c4e849582d7d5eacc99b9b8a
Kenneth Lim <kennethlimcp@gmail.com>
2020-06-27 20:32:09 +0800

    add: filter via_bot message
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
+from telegram.ext import BaseFilter
 import pandas as pd
 
 def food_recommendation(csv_path):
     food_db = pd.read_csv(csv_path, delimiter = ",")
 
     rand_food_row = food_db.sample()
-    print(rand_food_row)
     rand_food = rand_food_row["Name of food"].values[0]
 
     return rand_food
+
+class Filter_via_bot(BaseFilter):
+    def filter(self, message):
+        if not message.via_bot:
+            return False
